edac7d9c50c2b4c3ad411a6350321af896285e3b
EC2 Default User <ec2-user@ip-172-31-21-64.us-east-2.compute.internal>
2018-03-07 16:37:16 +0000

    testing teapot anim

--- DigitalHomeWork5/js/Preloader.js
+++ DigitalHomeWork5/js/Preloader.js
@@ -27,6 +27,7 @@ GameStates.makePreloader = function( game ) {
             game.load.atlas('playButton', 'assets/play_button.png', 'assets/play_button.json');
             game.load.audio('titleMusic', ['assets/Poppers and Prosecco.mp3']);
             //	+ lots of other required assets here
+            game.load.spritesheet('teapot', "assets/TeaPot.png",32,32);
             game.load.image( 'logo', 'assets/phaser.png' );
         },
     
