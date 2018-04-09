e3b1015213125169b9eba34c4ca70edf8b3a01e4
EC2 Default User <ec2-user@ip-172-31-21-64.us-east-2.compute.internal>
2018-04-09 15:10:15 +0000

    asfd

--- DigitalHomeWork7/js/Game.js
+++ DigitalHomeWork7/js/Game.js
@@ -188,10 +188,8 @@ GameStates.makeGame = function( game, shared ) {
             
             drawWall(500,500,50,50);
             drawWall(100,100,150,50);
-            //game.world.setBounds(0,0,2000,4000);
-            
-            drawWall(1,1,1998, 3998);
-            game.camera.follow(player);
+            game.world.setBounds(0,0,2000,4000);
+            drawWall(1,1,1023, 767);
             
         },
     
