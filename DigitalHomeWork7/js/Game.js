bbda70c3e62a4ae440a1fc74688ca6e72ad2d263
EC2 Default User <ec2-user@ip-172-31-21-64.us-east-2.compute.internal>
2018-04-09 14:02:55 +0000

    asdfas

--- DigitalHomeWork7/js/Game.js
+++ DigitalHomeWork7/js/Game.js
@@ -61,7 +61,12 @@ GameStates.makeGame = function( game, shared ) {
         totalLineLength+= smallestIntersectionLine.length;
         let forwardLine = smallestIntersectionLine;  
         //Away from the the player
-        lineExtrapolated = new Phaser.Line(player.x,player.y,(player.x + -2000 * Math.cos(lineToPointer.angle),(player.y + -2000*Math.sin(lineToPointer.angle))));
+        let flippedLineAngle = lineToPointer.angle * (180/Math.PI);
+        flippedLineAngle = (flippedLineAngle+180) % 360;
+        flippedLineAngle = flippedLineAngle * (Math.PI/180);
+        console.log(lineToPointer.angle);
+        confirm.log(flippedLineAngle);
+        lineExtrapolated = new Phaser.Line(player.x,player.y,(player.x + -2000 * Math.cos(flippedLineAngle),(player.y + -2000*Math.sin(flippedLineAngle))));
         smallestIntersectionDistance = 100000;
         smallestIntersectionLine = 0;
         walls.forEach(function(wall){
